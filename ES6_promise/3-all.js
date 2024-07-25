a1efdd455e2193786e65ed8906f64d062fa5a947
HolbieEnjoyer <8698@holbertonstudents.com>
2024-07-25 13:16:48 +0400

    Next task

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,1 +1,11 @@
 /* eslint-disable */
+import {uploadPhoto} from './utils.js';
+import {createUser} from "./utils.js";
+
+export default function handleProfileSignup() {
+    Promise.all([uploadPhoto(), createUser()]).then((values) => {
+        console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
+    }).catch(() => {
+        console.log('Signup system offline');
+    });
+}
